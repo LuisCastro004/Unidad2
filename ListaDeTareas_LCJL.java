48e0231535c8a63e2a81d0b25012e81ff27027f4
LuisCastro004 <143724794+LuisCastro004@users.noreply.github.com>
2023-09-05 08:45:26 -0600

    Add files via upload

---
+++ ListaDeTareas_LCJL.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaDeTareas {
+   
+    public static void main(String[] args){
+        String Tarea1 = "Ejercicios de calculo diferencial";
+        String Tarea2 = "Exposicion de etica";
+        String Tarea3 = "Acomodar materiales de escuela";
+        String Tarea4 = "Investigación del tema de clase";
+        String Tarea5 = "Practica programacion";
+        
+        System.out.println("Primera tarea: " + Tarea1);
+        System.out.println("Segunda tarea: " + Tarea2);
+        System.out.println("Tercer tarea: " + Tarea3);
+        System.out.println("Cuarta tarea: " + Tarea4);
+        System.out.println("Quinta tarea: " + Tarea5);
+        
+    }
+}
